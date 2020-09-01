c55641e5a56485028959d447edf933edb586454c
SujoyBasak <sujoyr723@gmail.com>
2020-09-01 15:48:29 +0530

    Source Code Added

---
+++ ExpenseManagement/Controllers/ExpenseController.cs
@@ -0,0 +1,35 @@
+﻿using ExpenseManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpenseManagement.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        // GET: Expense
+        ExpenseContext con = new ExpenseContext();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            
+            return View(con.EMS_DB.ToList());
+            
+        }
+        public ActionResult AddExpense()
+        {
+            ViewBag.msg = "Expense added Successfully";
+            var x = new ExpenseViewModel();
+            return View(x);
+        }
+        [HttpPost]
+        public ActionResult AddExpense(EMS_DB expense)
+        {
+            con.EMS_DB.Add(expense);
+            con.SaveChanges();
+            return View("AddExpense");
+        }
+    }
+}
---
+++ ExpenseManagement/Models/ExpenseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExpenseManagement.Models;
+
+namespace ExpenseManagement.Models
+{
+    public class ExpenseViewModel
+    {
+        public EMS_DB Expense { get; set; }
+
+
+        public IEnumerable<SelectListItem> ExpenseTypeList
+        {
+            get
+            {
+                return new List<SelectListItem>()
+                     {
+                       new SelectListItem { Text = "Food Expense", Value = "Food Expense" },
+                        new SelectListItem { Text = "Health Expense", Value = "Health Expense" },
+                        new SelectListItem { Text = "Travel Expense", Value = "Travel Expense" },
+                        new SelectListItem { Text = "Entertainment Expense", Value = "Entertainment Expense" },
+                     };
+            }
+            set
+            {
+
+            }
+
+        }
+    }
+}
